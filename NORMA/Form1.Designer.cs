324fcd742589cfb923e71c34d4ccf71b634e1171
JoseFabela <jfabela522@gmail.com>
2024-11-19 19:07:15 -0600

    Agregar archivos de proyecto.

---
+++ NORMA/Form1.Designer.cs
@@ -0,0 +1,419 @@
+﻿namespace NORMA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.panelLuz = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.panel11 = new System.Windows.Forms.Panel();
+            this.panel12 = new System.Windows.Forms.Panel();
+            this.panel13 = new System.Windows.Forms.Panel();
+            this.panel14 = new System.Windows.Forms.Panel();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.checkBox10 = new System.Windows.Forms.CheckBox();
+            this.checkBox11 = new System.Windows.Forms.CheckBox();
+            this.checkBox12 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(148, 167);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(162, 20);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Luz de Cuarto De Niño";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(353, 167);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(133, 20);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Luz de Dormitorio";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(539, 167);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(162, 20);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Luz de Cuarto De Niña";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(148, 289);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(103, 20);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "Luz de Baño";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(353, 289);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(113, 20);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "Luz de Cocina";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(539, 289);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(99, 20);
+            this.checkBox6.TabIndex = 5;
+            this.checkBox6.Text = "Luz de Sala";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            this.checkBox6.CheckedChanged += new System.EventHandler(this.checkBox6_CheckedChanged);
+            // 
+            // panelLuz
+            // 
+            this.panelLuz.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panelLuz.Location = new System.Drawing.Point(165, 136);
+            this.panelLuz.Name = "panelLuz";
+            this.panelLuz.Size = new System.Drawing.Size(33, 25);
+            this.panelLuz.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Location = new System.Drawing.Point(376, 136);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(33, 25);
+            this.panel1.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel2.Location = new System.Drawing.Point(549, 136);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(33, 25);
+            this.panel2.TabIndex = 7;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel3.Location = new System.Drawing.Point(549, 258);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(33, 25);
+            this.panel3.TabIndex = 7;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel4.Location = new System.Drawing.Point(376, 258);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(33, 25);
+            this.panel4.TabIndex = 7;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel5.Location = new System.Drawing.Point(165, 258);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(33, 25);
+            this.panel5.TabIndex = 7;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.panel6.Location = new System.Drawing.Point(755, 329);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(33, 25);
+            this.panel6.TabIndex = 8;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.Red;
+            this.panel7.Location = new System.Drawing.Point(755, 374);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(33, 25);
+            this.panel7.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(642, 329);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "ENCENDIDO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(642, 383);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "ERROR";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(642, 425);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 16);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "APAGADO";
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.DarkGray;
+            this.panel8.Location = new System.Drawing.Point(755, 413);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(33, 25);
+            this.panel8.TabIndex = 10;
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel9.Location = new System.Drawing.Point(165, 315);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(33, 25);
+            this.panel9.TabIndex = 15;
+            // 
+            // panel10
+            // 
+            this.panel10.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel10.Location = new System.Drawing.Point(376, 315);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(33, 25);
+            this.panel10.TabIndex = 16;
+            // 
+            // panel11
+            // 
+            this.panel11.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel11.Location = new System.Drawing.Point(549, 315);
+            this.panel11.Name = "panel11";
+            this.panel11.Size = new System.Drawing.Size(33, 25);
+            this.panel11.TabIndex = 17;
+            // 
+            // panel12
+            // 
+            this.panel12.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel12.Location = new System.Drawing.Point(549, 193);
+            this.panel12.Name = "panel12";
+            this.panel12.Size = new System.Drawing.Size(33, 25);
+            this.panel12.TabIndex = 18;
+            // 
+            // panel13
+            // 
+            this.panel13.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel13.Location = new System.Drawing.Point(376, 193);
+            this.panel13.Name = "panel13";
+            this.panel13.Size = new System.Drawing.Size(33, 25);
+            this.panel13.TabIndex = 19;
+            // 
+            // panel14
+            // 
+            this.panel14.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel14.Location = new System.Drawing.Point(165, 193);
+            this.panel14.Name = "panel14";
+            this.panel14.Size = new System.Drawing.Size(33, 25);
+            this.panel14.TabIndex = 14;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(539, 346);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(113, 20);
+            this.checkBox7.TabIndex = 13;
+            this.checkBox7.Text = "Clima de Sala";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            this.checkBox7.CheckedChanged += new System.EventHandler(this.checkBox7_CheckedChanged);
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(353, 346);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(127, 20);
+            this.checkBox8.TabIndex = 12;
+            this.checkBox8.Text = "Clima de Cocina";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            this.checkBox8.CheckedChanged += new System.EventHandler(this.checkBox8_CheckedChanged);
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Location = new System.Drawing.Point(148, 346);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(117, 20);
+            this.checkBox9.TabIndex = 11;
+            this.checkBox9.Text = "Clima de Baño";
+            this.checkBox9.UseVisualStyleBackColor = true;
+            this.checkBox9.CheckedChanged += new System.EventHandler(this.checkBox9_CheckedChanged);
+            // 
+            // checkBox10
+            // 
+            this.checkBox10.AutoSize = true;
+            this.checkBox10.Location = new System.Drawing.Point(539, 224);
+            this.checkBox10.Name = "checkBox10";
+            this.checkBox10.Size = new System.Drawing.Size(176, 20);
+            this.checkBox10.TabIndex = 10;
+            this.checkBox10.Text = "Clima de Cuarto De Niña";
+            this.checkBox10.UseVisualStyleBackColor = true;
+            this.checkBox10.CheckedChanged += new System.EventHandler(this.checkBox10_CheckedChanged);
+            // 
+            // checkBox11
+            // 
+            this.checkBox11.AutoSize = true;
+            this.checkBox11.Location = new System.Drawing.Point(353, 224);
+            this.checkBox11.Name = "checkBox11";
+            this.checkBox11.Size = new System.Drawing.Size(147, 20);
+            this.checkBox11.TabIndex = 9;
+            this.checkBox11.Text = "Clima de Dormitorio";
+            this.checkBox11.UseVisualStyleBackColor = true;
+            this.checkBox11.CheckedChanged += new System.EventHandler(this.checkBox11_CheckedChanged);
+            // 
+            // checkBox12
+            // 
+            this.checkBox12.AutoSize = true;
+            this.checkBox12.Location = new System.Drawing.Point(148, 224);
+            this.checkBox12.Name = "checkBox12";
+            this.checkBox12.Size = new System.Drawing.Size(176, 20);
+            this.checkBox12.TabIndex = 8;
+            this.checkBox12.Text = "Clima de Cuarto De Niño";
+            this.checkBox12.UseVisualStyleBackColor = true;
+            this.checkBox12.CheckedChanged += new System.EventHandler(this.checkBox12_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel10);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel11);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel12);
+            this.Controls.Add(this.panel13);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel14);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.checkBox9);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.checkBox10);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.checkBox11);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.checkBox12);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelLuz);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.Panel panelLuz;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Panel panel11;
+        private System.Windows.Forms.Panel panel12;
+        private System.Windows.Forms.Panel panel13;
+        private System.Windows.Forms.Panel panel14;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.CheckBox checkBox10;
+        private System.Windows.Forms.CheckBox checkBox11;
+        private System.Windows.Forms.CheckBox checkBox12;
+    }
+}
+
---
+++ NORMA/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NORMA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //Bitmap img = new Bitmap(Application.StartupPath + @"C:\Users\junio\Downloads\fondocasa.jpeg");
+            Bitmap img = new Bitmap(@"C:\Users\junio\Downloads\fondocasa.jpeg");
+
+            this.BackgroundImage = img;
+            this.BackgroundImageLayout = ImageLayout.Zoom;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                panelLuz.BackColor = Color.Green; // Encendido
+            }
+            else
+            {
+                panelLuz.BackColor = Color.Gray; // Apagado
+            }
+            CheckAllCheckboxes();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                panel1.BackColor = Color.Green; // Encendido
+            }
+            else
+            {
+                panel1.BackColor = Color.Gray; // Apagado
+            }
+            CheckAllCheckboxes();
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked)
+            {
+                panel2.BackColor = Color.Green; // Encendido
+            }
+            else
+            {
+                panel2.BackColor = Color.Gray; // Apagado
+            }
+            CheckAllCheckboxes();
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox4.Checked)
+            {
+                panel5.BackColor = Color.Green; // Encendido
+            }
+            else
+            {
+                panel5.BackColor = Color.Gray; // Apagado
+            }
+            CheckAllCheckboxes();
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox5.Checked)
+            {
+                panel4.BackColor = Color.Green; // Encendido
+            }
+            else
+            {
+                panel4.BackColor = Color.Gray; // Apagado
+            }
+            CheckAllCheckboxes();
+        }
+
+        private void checkBox6_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox6.Checked)
+            {
+                panel3.BackColor = Color.Green; // Encendido
+            }
+            else
+            {
+                panel3.BackColor = Color.Gray; // Apagado
+            }
+            CheckAllCheckboxes();
+        }
+
+        private void checkBox12_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox12.Checked)
+            {
+                panel14.BackColor = Color.Green; // Encendido
+            }
+            else
+            {
+                panel14.BackColor = Color.Gray; // Apagado
+            }
+            CheckAllCheckboxes();
+        }
+
+        private void checkBox11_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox11.Checked)
+            {
+                panel13.BackColor = Color.Green; // Encendido
+            }
+            else
+            {
+                panel13.BackColor = Color.Gray; // Apagado
+            }
+            CheckAllCheckboxes();
+        }
+
+        private void checkBox10_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox10.Checked)
+            {
+                panel12.BackColor = Color.Green; // Encendido
+            }
+            else
+            {
+                panel12.BackColor = Color.Gray; // Apagado
+            }
+            CheckAllCheckboxes();
+        }
+
+        private void checkBox9_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox9.Checked)
+            {
+                panel9.BackColor = Color.Green; // Encendido
+            }
+            else
+            {
+                panel9.BackColor = Color.Gray; // Apagado
+            }
+            CheckAllCheckboxes();
+        }
+
+        private void checkBox8_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox8.Checked)
+            {
+                panel10.BackColor = Color.Green; // Encendido
+            }
+            else
+            {
+                panel10.BackColor = Color.Gray; // Apagado
+            }
+            CheckAllCheckboxes();
+        }
+
+        private void checkBox7_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox7.Checked)
+            {
+                panel11.BackColor = Color.Green; // Encendido
+            }
+            else
+            {
+                panel11.BackColor = Color.Gray; // Apagado
+            }
+            CheckAllCheckboxes();
+        }
+        // Nueva función para verificar todos los checkboxes
+        private void CheckAllCheckboxes()
+        {
+            // Lista de todos los checkboxes en el formulario
+            var checkboxes = new List<CheckBox> { checkBox1, checkBox2, checkBox3, checkBox4, checkBox5, checkBox6,
+                                          checkBox7, checkBox8, checkBox9, checkBox10, checkBox11, checkBox12 };
+
+            // Verifica si todos están marcados
+            if (checkboxes.All(cb => cb.Checked))
+            {
+                // Cambia todos los paneles a rojo
+                panelLuz.BackColor = Color.Red;
+                panel1.BackColor = Color.Red;
+                panel2.BackColor = Color.Red;
+                panel3.BackColor = Color.Red;
+                panel4.BackColor = Color.Red;
+                panel5.BackColor = Color.Red;
+                panel9.BackColor = Color.Red;
+                panel10.BackColor = Color.Red;
+                panel11.BackColor = Color.Red;
+                panel12.BackColor = Color.Red;
+                panel13.BackColor = Color.Red;
+                panel14.BackColor = Color.Red;
+            }
+        }
+    }
+}
